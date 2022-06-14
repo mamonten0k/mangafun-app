03a2825ad20fbb8c27335144a6d6c189e4c884a7
mamonten0k <asdnbvb123@gmail.com>
2022-06-15 00:13:16 +0300

    feat: add content for test purposes

---
+++ components/elements/index.js
@@ -0,0 +1,19 @@
+import { Accordion } from './Accordion/Accordion';
+import { AccordionItem } from './AccordionItem/AccordionItem';
+import { Card } from './Card/Card';
+import { Carousel } from './Carousel/Carousel';
+import { Comment } from './Comment/Comment';
+import { Dropdown } from './Dropdown/Dropdown';
+import { SearchBar } from './SearchBar/SearchBar';
+import { Tabs } from './Tabs/Tabs';
+
+export {
+  Accordion,
+  AccordionItem,
+  Card,
+  Carousel,
+  Comment,
+  Dropdown,
+  SearchBar,
+  Tabs,
+};
