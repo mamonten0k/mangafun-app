b9452e52bce9caed9d52ca7e7e8a8b5cca724160
mamonten0k <asdnbvb123@gmail.com>
2022-06-29 11:30:41 +0300

    refractor: make styles assignment clearer

--- components/elements/Tabs/Tabs.jsx
+++ components/elements/Tabs/Tabs.jsx
@@ -3,10 +3,15 @@ import { useRouter } from 'next/router';
 import { slugify } from '../../../utils/slugify';
 import styles from './Tabs.module.scss';
 
-//Partly copypasted from https://github.com/selfteachme/0025-react-tabbed-component
-const Tabs = ({ children, initialTab, flow, withBg }) => {
+const Tabs = ({ children, initialTab, withBg }) => {
   const [activeTab, setActiveTab] = useState(children[0].props.label);
   const router = useRouter();
+  // не то
+  const config = {
+    layout: [styles.layout, withBg && styles['layout-bg']]
+      .filter((item) => item)
+      .join(' '),
+  };
 
   const handleClick = (e, newActiveTab) => {
     e.preventDefault();
@@ -26,25 +31,19 @@ const Tabs = ({ children, initialTab, flow, withBg }) => {
   }, [activeTab]);
 
   return (
-    <div
-      className={`${styles[`layout-${flow}`]} ${
-        withBg ? styles[`layout-bg`] : ''
-      }`}>
-      <ul className={`${styles[`tabs-${flow}`]}`}>
-        {children.map((tab) => {
-          const label = tab.props.label;
-          return (
-            <li
-              className={`${styles[`tab-${flow}`]} ${
-                slugify(label) == activeTab ? styles[`tab-${flow}--active`] : ''
-              }`}
-              key={label}>
-              <a href='#' onClick={(e) => handleClick(e, label)}>
-                {label}
-              </a>
-            </li>
-          );
-        })}
+    <div className={config.layout}>
+      <ul className={styles.tabs}>
+        {children.map((tab) => (
+          <li
+            className={`${styles.tab} ${
+              slugify(tab.props.label) == activeTab ? styles['tab-active'] : ''
+            }`}
+            key={tab.props.label}>
+            <a href='#' onClick={(e) => handleClick(e, tab.props.label)}>
+              {tab.props.label}
+            </a>
+          </li>
+        ))}
       </ul>
       {children.map((item) => {
         if (slugify(item.props.label) == activeTab) return <>{item}</>;
